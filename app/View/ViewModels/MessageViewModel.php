2d34c0cfc32539ec282a10f17d7916f4ad11753b
Sasha <sashapozhidaev07@gmail.com>
2024-06-06 00:13:25 +0500

    edit context

--- app/View/ViewModels/MessageViewModel.php
+++ app/View/ViewModels/MessageViewModel.php
@@ -15,14 +15,13 @@ class MessageViewModel extends ViewModel
 
     public function getLastMessagesInText(): string
     {
-        $messages = Message::where('user_id', auth()->user()->id)->limit(10)->get();
-        $text = '';
+        $messages = Message::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->limit(20)->get();
+        $text = 'Последнии проблемы которые писал подросток:
+            (учитывай их и не спрашивай заного что случилось если он уже описал что у него за проблема)';
 
         foreach ($messages as $message) {
             if ($message->is_user) {
-                $text .= ' Сообщение подростка: ' . $message->message;
-            } else {
-                $text .= ' Твое сообщение: ' . $message->message;
+                $text .= '...' . $message->message;
             }
         }
 
